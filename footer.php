66da98a1cf38bb6be184b1578bc80b6fd211f057
Antonis Flangofas <a.flangofas@qobo.biz>
2016-09-19 14:56:44 +0300

    Created footer

--- footer.php
+++ footer.php
@@ -10,13 +10,43 @@
  */
 
 ?>
-	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'material-design-dentistry' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'material-design-dentistry' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'material-design-dentistry' ), 'material-design-dentistry', '<a href="http://underscores.me/" rel="designer">Underscores.me</a>' ); ?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
+	<footer class="page-footer">
+		<div class="container">
+			<div class="row">
+				<div class="col l4 s12">
+					<h6>NAVIGATION LINKS</h6>
+					<ul>
+						<li><a class="grey-text text-darken-4" href="#!">Link 1</a></li>
+						<li><a class="grey-text text-darken-4" href="#!">Link 2</a></li>
+						<li><a class="grey-text text-darken-4" href="#!">Link 3</a></li>
+						<li><a class="grey-text text-darken-4" href="#!">Link 4</a></li>
+					</ul>
+				</div>
+				<div class="col l4 s12">
+					<h6>CONTACT US</h6>
+					<p><i class="tiny material-icons">location_on</i> Agias Zonis 29, 1st Floor</p>
+					<p><i class="tiny material-icons">call</i> 25 55 8899</p>
+					<p><i class="tiny material-icons">print</i> 25 55 8899</p>
+					<p><i class="tiny material-icons">email</i> <a class="grey-text text-darken-4" href="mailto:info@drleonidou.com">info@drleonidou.com</a></p>
+				</div>
+				<div class="col l4 s12">
+					<h6>SOCIAL MEDIA</h6>
+					<ul class="list-inline">
+						<li><a href="#"><img src="<?php echo get_template_directory_uri() . '/img/fb-icon.png'; ?>"></a></li>
+						<li><a href="#"><img src="<?php echo get_template_directory_uri() . '/img/gplus-icon.png'; ?>"></a></li>
+						<li><a href="#"><img src="<?php echo get_template_directory_uri() . '/img/in-icon.png'; ?>"></a></li>
+					</ul>
+					<div class="fb-like" data-href="https://www.facebook.com/Dr.Leonidou" data-layout="standard" data-action="like" data-size="small" data-show-faces="true" data-share="true"></div>
+				</div>
+			</div>
+		</div>
+		<div class="footer-copyright">
+			<div class="container grey-text text-darken-4">
+			© <?php echo date('Y') ?> Copyright Text
+			<a class="grey-text text-darken-4 right" href="#!">Developed by Antonis Flangofas</a>
+			</div>
+		</div>
+	</footer>
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
--- header.php
+++ header.php
@@ -20,6 +20,14 @@
 </head>
 
 <body <?php body_class(); ?>>
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.7";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'material-design-dentistry' ); ?></a>
 
@@ -34,7 +42,7 @@
 		<nav>
 			<div class="container">
 				<div class="nav-wrapper">
-					<a href="#" class="brand-logo center"><img src="<?php echo get_template_directory_uri() . '/images/logo.png'; ?>" alt="Site logo"></a>
+					<a href="#" class="brand-logo center"><img src="<?php echo get_template_directory_uri() . '/img/logo.png'; ?>" alt="Site logo"></a>
 					<ul id="nav-mobile" class="left hide-on-med-and-down">
 						<li><a class="dropdown-button" href="#!" data-activates="dropdown1">OUR DOCTORS<i class="material-icons right">arrow_drop_down</i></a></li>
 						<li><a href="badges.html">OUR CLINIC</a></li>
