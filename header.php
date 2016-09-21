5498633b573fc39988fadf90f596aa6f45fac5aa
Antonis Flangofas <a.flangofas@qobo.biz>
2016-09-21 23:46:31 +0300

    Load Roboto Condensed

--- header.php
+++ header.php
@@ -15,6 +15,7 @@
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
 <link rel="profile" href="http://gmpg.org/xfn/11">
+<link href="https://fonts.googleapis.com/css?family=Roboto+Condensed:300,400,700" rel="stylesheet">
 
 <?php wp_head(); ?>
 </head>
