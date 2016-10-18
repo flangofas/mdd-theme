2e0ff64b7535e477a615465444d83a89f19c1189
Antonis Flangofas <antonisflags@gmail.com>
2016-10-19 02:06:05 +0300

    Created contact us page

--- js/theme/init.js
+++ js/theme/init.js
@@ -5,4 +5,13 @@ jQuery(document).ready(function ($) {
     $('.slider').slider({full_width: true, height: 700, indicators: false});
     $('.carousel').carousel();
     $('.materialboxed').materialbox();
+    //Removes span wrapper and br tags in the form of CF7 plugin
+    //@link: http://gmkhussain.blogspot.com.cy/2015/12/remove-span-elements-from-contact-form.html
+    var cf7input = $( ".wpcf7-form-control" );
+    if (cf7input.parent().is("span")) {
+        cf7input.unwrap();
+    } else {
+        cf7input.wrap( "<div></div>" );
+    }
+	$('.input-field br').remove();
 });
---
+++ page-contact-us.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Template Name: Horizontal Card
+ * The template for displaying a horizontal card.
+ *
+ * @package Material_Design_Dentistry
+ *
+ */
+get_header();
+?>
+    <div id="primary">
+        <main id="main" class="site-main" role="main">
+            <?php
+            while ( have_posts() ) : the_post();
+                get_template_part( 'template-parts/main', 'title' );
+            ?>
+            <div class="content-area">
+                <div class="container">
+                    <div class="row">
+                        <?php
+                        /**
+                         * @todo: Should be taken from custom fields rather than hardcoded shortcodes
+                         */
+                        ?>
+                        <div class="col l5 s12"><?php echo do_shortcode('[wpgmza id="1"]'); ?></div>
+                        <div class="col offset-l1 l6 s12"><?php echo do_shortcode('[contact-form-7 id="3" title="Contact form 1"]'); ?></div>
+                    </div>
+                </div>
+            </div>
+            <?php
+                // If comments are open or we have at least one comment, load up the comment template.
+                if ( comments_open() || get_comments_number() ) :
+                    comments_template();
+                endif;
+
+            endwhile; // End of the loop.
+            ?>
+        </main><!-- #main -->
+    </div><!-- #primary -->
+<?php
+get_footer();
+?>
