547223972859a098f1afd5df0ba69b291fbf7374
Antonis Flangofas <antonisflags@gmail.com>
2016-11-10 23:45:17 +0200

    Fixed background

--- single.php
+++ single.php
@@ -9,27 +9,29 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+    <div id="primary" class="content-area">
+        <main id="main" class="site-main" role="main">
             <div class="content-area">
                 <div class="container">
-                	<div class="row">
-                		<div class="col s12">
-							<?php
-							while ( have_posts() ) : the_post();
-								if (has_post_thumbnail()) : ?>
-								    <img class="responsive-img" src="<?php echo get_the_post_thumbnail_url(); ?>">
-								<?php
-								endif;
-								the_content();
-							endwhile; // End of the loop.
-							?>
-						</div><!-- col -->
-					</div><!-- row -->
-				</div><!-- content-area -->
-			</div><!-- container -->
-		</main><!-- #main -->
-	</div><!-- #primary -->
+                    <div class="row">
+                        <div class="col s12">
+                            <div class="main-content breathing-space">
+                            <?php
+                            while ( have_posts() ) : the_post();
+                                if (has_post_thumbnail()) : ?>
+                                    <img class="responsive-img" src="<?php echo get_the_post_thumbnail_url(); ?>">
+                                <?php
+                                endif;
+                                the_content();
+                            endwhile; // End of the loop.
+                            ?>
+                            </div>
+                        </div><!-- col -->
+                    </div><!-- row -->
+                </div><!-- content-area -->
+            </div><!-- container -->
+        </main><!-- #main -->
+    </div><!-- #primary -->
 
 <?php
 get_sidebar();
