dde520eb1aea4c2dc646e3f3ae57a9ad36921680
Antonis Flangofas <antonisflags@gmail.com>
2016-11-13 03:58:42 +0200

    Created mobile, desktop navbar

--- header.php
+++ header.php
@@ -33,27 +33,6 @@
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'material-design-dentistry' ); ?></a>
 
 	<header id="masthead" class="site-header" role="banner">
-		<!-- Dropdown Structure -->
-		<ul id="our-doctors" class="dropdown-content">
-			<li><a href="/orthodontist/">Orthodontist</a></li>
-			<li><a href="/paediatric/">Paediatric</a></li>
-		</ul>
-		<nav>
-			<div class="container">
-				<div class="nav-wrapper">
-					<a href="/" class="brand-logo center"><img src="<?php echo get_template_directory_uri() . '/img/logo.png'; ?>" alt="Site logo"></a>
-					<ul id="nav-mobile" class="left hide-on-med-and-down">
-						<li><a class="dropdown-button" href="#!" data-beloworigin="true" data-activates="our-doctors">OUR DOCTORS<i class="material-icons right">arrow_drop_down</i></a></li>
-						<li><a href="/our-clinic/">OUR CLINIC</a></li>
-						<li><a href="/our-treatments/">TREATMENTS</a></li>
-					</ul>
-					<ul id="nav-mobile" class="right hide-on-med-and-down">
-						<li><a href="/blog/">ARTICLES</a></li>
-						<li><a href="/videos/">VIDEOS</a></li>
-						<li><a href="/faq/">FAQ</a></li>
-						<li><a href="/contact-us/">CONTACT US</a></li>
-					</ul>
-				</div>
-			</div>
-		</nav>
+		<?php get_template_part('template-parts/navbar', 'desktop'); ?>
+		<?php get_template_part('template-parts/navbar', 'mobile'); ?>
 	</header><!-- #masthead -->
---
+++ template-parts/navbar-mobile.php
@@ -0,0 +1,21 @@
+<ul id="slide-out" class="side-nav">
+	<li class="no-padding">
+		<ul class="collapsible collapsible-accordion">
+			<li>
+				<a class="collapsible-header">OUR DOCTORS<i class="material-icons">arrow_drop_down</i></a>
+				<div class="collapsible-body">
+					<ul>
+						<li><a href="/orthodontist/">Orthodontist</a></li>
+						<li><a href="/paediatric/">Paediatric</a></li>
+					</ul>
+				</div>
+			</li>
+		</ul>
+	</li>
+	<li><a href="/our-clinic/">OUR CLINIC</a></li>
+	<li><a href="/our-treatments/">TREATMENTS</a></li>
+	<li><a href="/blog/">ARTICLES</a></li>
+	<li><a href="/videos/">VIDEOS</a></li>
+	<li><a href="/faq/">FAQ</a></li>
+	<li><a href="/contact-us/">CONTACT US</a></li>
+</ul>
