b98e14271e14ed951c474825088dd930f90dd245
Antonis Flangofas <antonisflags@gmail.com>
2017-02-28 17:10:13 +0200

    Enabled fixed navbar

--- template-parts/navbar-desktop.php
+++ template-parts/navbar-desktop.php
@@ -1,32 +1,34 @@
-<!-- Dropdown Structure -->
-<ul id="our-doctors" class="dropdown-content">
-	<li><a href="/our-doctors/orthodontist/">Orthodontist</a></li>
-	<li><a href="/our-doctors/paediatric-dentist/">Paediatric Dentist</a></li>
-</ul>
-<ul id="treatments" class="dropdown-content">
-	<li><a href="/treatments/orthodontics/">Orthodontics</a></li>
-	<li><a href="/treatments/paediatric-dentistry/">Paediatric Dentistry</a></li>
-</ul>
-<ul id="faq" class="dropdown-content">
-	<li><a href="/faq/orthodontics/">Orthodontics</a></li>
-	<li><a href="/faq/paediatric-dentistry/">Paediatric Dentistry</a></li>
-</ul>
-<nav>
-	<div class="container">
-		<div class="nav-wrapper">
-			<a href="#" data-activates="slide-out" class="button-collapse"><i class="material-icons">menu</i></a>
-			<a href="/" class="brand-logo center"><img src="<?php echo get_template_directory_uri() . '/img/logo.png'; ?>" alt="Site logo"></a>
-			<ul id="nav-mobile" class="left hide-on-med-and-down">
-				<li><a class="dropdown-button" href="#!" data-beloworigin="true" data-activates="our-doctors">OUR DOCTORS<i class="material-icons right">arrow_drop_down</i></a></li>
-				<li><a href="/our-clinic/">OUR CLINIC</a></li>
-				<li><a class="dropdown-button" href="#!" data-beloworigin="true" data-activates="treatments">TREATMENTS<i class="material-icons right">arrow_drop_down</i></a></li>
-			</ul>
-			<ul id="nav-mobile" class="right hide-on-med-and-down">
-				<li><a href="/blog/">ARTICLES</a></li>
-				<li><a href="/videos/">VIDEOS</a></li>
-				<li><a class="dropdown-button" href="#!" data-beloworigin="true" data-activates="faq">FAQ <i class="material-icons right">arrow_drop_down</i></a></li>
-				<li><a href="/contact-us/">CONTACT US</a></li>
-			</ul>
+<div class="navbar-fixed">
+	<nav>
+		<div class="container">
+			<div class="nav-wrapper">
+				<a href="#" data-activates="slide-out" class="button-collapse"><i class="material-icons">menu</i></a>
+				<a href="/" class="brand-logo center"><img src="<?php echo get_template_directory_uri() . '/img/logo.png'; ?>" alt="Site logo"></a>
+				<ul id="nav-mobile" class="left hide-on-med-and-down">
+					<li><a class="dropdown-button" href="#!" data-beloworigin="true" data-activates="our-doctors">OUR DOCTORS<i class="material-icons right">arrow_drop_down</i></a></li>
+					<li><a href="/our-clinic/">OUR CLINIC</a></li>
+					<li><a class="dropdown-button" href="#!" data-beloworigin="true" data-activates="treatments">TREATMENTS<i class="material-icons right">arrow_drop_down</i></a></li>
+				</ul>
+				<ul id="nav-mobile" class="right hide-on-med-and-down">
+					<li><a href="/blog/">ARTICLES</a></li>
+					<li><a href="/videos/">VIDEOS</a></li>
+					<li><a class="dropdown-button" href="#!" data-beloworigin="true" data-activates="faq">FAQ <i class="material-icons right">arrow_drop_down</i></a></li>
+					<li><a href="/contact-us/">CONTACT US</a></li>
+				</ul>
+			</div>
 		</div>
-	</div>
-</nav>
+	</nav>
+	<!-- Dropdown Structure -->
+	<ul id="our-doctors" class="dropdown-content">
+		<li><a href="/our-doctors/orthodontist/">Orthodontist</a></li>
+		<li><a href="/our-doctors/paediatric-dentist/">Paediatric Dentist</a></li>
+	</ul>
+	<ul id="treatments" class="dropdown-content">
+		<li><a href="/treatments/orthodontics/">Orthodontics</a></li>
+		<li><a href="/treatments/paediatric-dentistry/">Paediatric Dentistry</a></li>
+	</ul>
+	<ul id="faq" class="dropdown-content">
+		<li><a href="/faq/orthodontics/">Orthodontics</a></li>
+		<li><a href="/faq/paediatric-dentistry/">Paediatric Dentistry</a></li>
+	</ul>
+</div>
